300e83599872e99d890b33fb8f4ae8cc49baa80a
Galih Wicaksono <galih.wicaksono@ericsson.com>
2019-05-10 11:45:27 +0200

    commit setup.py all are commited

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kpiGenerator',
+    version='0.1.0',
+    description='KPI Generator for Ericsson Magic Leap Hackathon 2019 Project',
+    python_requires=">=3.5.0",
+    packages=find_packages(),
+    install_requires=["psutil", "requests"],
+    entry_points = {
+        "console_scripts": [
+            "kpiGenerator = kpiGenerator:main"
+        ]
+    }
+)
+
